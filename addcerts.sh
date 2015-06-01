e5ba7ea3b7afbd8152f12692cd9636bcc598f4c3
Phil <phil.prenz@gmail.com>
2015-06-01 15:10:10 -0400

    Delete addcerts.sh

--- addcerts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-1.save the certs /tmp/
-
-2. cd /opt/datalex/jdk/bin
-
-3. run ./keytool -import -trustcacerts -keystore /opt/datalex/jdk/jre/lib/security/cacerts -storepass changeit  -noprompt -alias  <usecertfilename> -file /tmp/<certfilename>.crt
